2ade40bf1026455a247cca42cc768124b21c948f
Bram Borggreve <borggreve@gmail.com>
2018-10-17 02:14:29 -0500

    wip

--- apps/admin/src/app/app.module.ts
+++ apps/admin/src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser'
+import { HttpClientModule } from '@angular/common/http'
 import { NgModule } from '@angular/core'
-
-import { AppComponent } from './app.component'
 import { NxModule } from '@nrwl/nx'
+import { AppComponent } from './app.component'
+import { AppService } from './app.service';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, NxModule.forRoot()],
-  providers: [],
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    HttpClientModule,
+    NxModule.forRoot(),
+  ],
+  providers: [AppService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ libs/core/src/lib/demo-items.ts
@@ -0,0 +1,18 @@
+export const items = [
+  {
+    name: 'Angular',
+    description: 'Angular is a platform to build apps.',
+  },
+  {
+    name: 'NestJS',
+    description: 'NestJS is a framework to build api`s.',
+  },
+  {
+    name: 'Nx',
+    description: 'Nx is a toolkit for enterprise Angular apps.',
+  },
+  {
+    name: 'Universal',
+    description: 'Universal adds Server Side Rendering to Angular apps.',
+  }
+]
